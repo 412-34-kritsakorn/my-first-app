b705e971b5551a382cbcab7e96b6ae5a5a7ba71c
412-34-kritsakorn <56436@yupparaj.ac.th>
2026-07-21 11:03:04 +0700

    Update convertYear.py

--- convertYear.py
+++ convertYear.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("แอปพลิเคชั่นแปลงปี พ.ศ. ค.ศ.")
+st.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
 
 bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง",value=2569)
 ce_yeae=bh_year-543
